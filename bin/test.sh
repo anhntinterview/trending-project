12f35a38302fbde6f188f5b64ff93f3c22b97703
anhntinteview <roberthost.vn@gmail.com>
2023-07-28 17:21:41 +0700

    test read bash file 5th

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,54 @@
 #!/usr/bin/env bash
 
-echo "IMAGE_NAME=$IMAGE_NAME"
-echo "CONTAINER_NAME=$CONTAINER_NAME"
-
-build_docker(){
-    docker build -t $(echo ${{ secrets.DOCKERHUB_USERNAME }})/$(echo $IMAGE_NAME):$(echo ${{ secrets.DOCKERHUB_TAG }} | head -c7) -f Dockerfile.app .
+err() {
+    echo $* >&2
 }
+
+usage() {
+    err "$(basename $0): [clear|run]"
+}
+
+clean() {
+    if [ "$(docker ps -a | grep -c ${{ env.CONTAINER_NAME }})" -gt 0 ]; then
+    docker stop ${{ env.CONTAINER_NAME }}
+    docker rm ${{ env.CONTAINER_NAME }}
+    docker image prune -a -f
+    echo "[---- Deleted container ${{ env.CONTAINER_NAME }}]"
+    else
+    echo "[---- Container with name: ${{ env.CONTAINER_NAME }}  doesn't exist. ]"
+    fi
+}
+
+pull(){
+    docker pull $(echo ${{ secrets.DOCKERHUB_USERNAME }})/$(echo $IMAGE_NAME):$(echo ${{ secrets.DOCKERHUB_TAG }} | head -c7)
+}
+
+launch() {
+    docker run --name ${{ env.CONTAINER_NAME }} -d -p 4000:4000 $(echo ${{ secrets.DOCKERHUB_USERNAME }})/$(echo $IMAGE_NAME):$(echo ${{ secrets.DOCKERHUB_TAG }} | head -c7)
+}
+
+execute() {
+    local task={1}
+    case ${task} in
+        clear)
+            clean
+            ;;
+        run)
+            pull
+            launch
+            ;;
+        *)
+            err "invalid task: ${task}"
+            usage
+            exit 1
+            ;;
+    esac
+}
+
+main() {
+    [ $# -ne 1 ] && { usage; exit 1; }
+    local task=${1}
+    execute ${task}
+}
+
+main $@
