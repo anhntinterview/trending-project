fc16b5a66fea73a5b3a3c34770548bf5182c2ff4
anhntinteview <roberthost.vn@gmail.com>
2023-07-28 17:05:13 +0700

    test read bash file 4th

--- bin/test.sh
+++ bin/test.sh
@@ -1,1 +1,8 @@
-echo 'Hello World!'
+#!/usr/bin/env bash
+
+echo "IMAGE_NAME=$IMAGE_NAME"
+echo "CONTAINER_NAME=$CONTAINER_NAME"
+
+build_docker(){
+    docker build -t $(echo ${{ secrets.DOCKERHUB_USERNAME }})/$(echo $IMAGE_NAME):$(echo ${{ secrets.DOCKERHUB_TAG }} | head -c7) -f Dockerfile.app .
+}
