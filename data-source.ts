0c4fe2615c1ac2368f479c528371879d1537caba
anhntinteview <roberthost.vn@gmail.com>
2023-08-13 23:17:31 +0700

    update .env

--- data-source.ts
+++ data-source.ts
@@ -11,23 +11,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_DATABASE,
   synchronize: true,
   logging: false,
-  // entities: [path.join(__dirname, './src/database/entity/*.*')],    
-    // entities: [__dirname + "src/database/entity/**/*.ts"],
-    // entities: ["src/database/entity/**/*.ts"],
-    entities: [TableA, TableB, LabEntity],
+  entities: [TableA, TableB, LabEntity],
   migrations: [],
   subscribers: []
 });
-// export const AppDataSource = new DataSource({
-//     type: "postgres",
-//     host: "localhost",
-//     port: 5432,
-//     username: "postgres",
-//     password: "221089abcdE%",
-//     database: "postgres",
-//     synchronize: true,
-//     logging: false,
-//     entities: ["src/database/entity/**/*.ts"],
-//     migrations: [],
-//     subscribers: [],
-// })
